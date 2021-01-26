2b408a999f0f04f3990e1f6b1830f7a86718e57f
Noé <noephilippe29@gmail.com>
2021-01-26 17:47:13 +0100

    maj header

--- site/main.js
+++ site/main.js
@@ -1,1 +1,15 @@
 
+//ANIMATION DE LA DIV NAVIGATION
+let header = document.querySelector('header');
+let navBar = document.querySelector('.navBar');
+
+//ANIMATION NAVIGATION
+window.addEventListener('scroll', () => {
+   if (window.scrollY > 60) {
+      header.classList.add('position');
+      navBar.classList.add('position');
+   } else {
+      navBar.classList.remove('position');
+      header.classList.remove('position');
+   }
+});

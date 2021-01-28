21bd8a85103be5c3da718709f4fa078c3b7b0695
Noé <noephilippe29@gmail.com>
2021-01-28 20:04:41 +0100

    maj navBar

--- focus/navBar.php
+++ focus/navBar.php
@@ -1,14 +1,21 @@
+
 <aside>
-   <div class="navBar">
-      <a href="index.php">Home</a>
-      <a href="actualite.php">Actualités</a>
-      <a class="navMenu">Sélections
-         <div class="menu">
-            <p href="#">Paysage</p>
-            <p href="#">Portrait</p>
-         </div>
-      </a>
-      <a href="presentation.php">Présentation</a>
-      <a class="navCompte" href="compte.php">Compte</a>
-   </div>
+   <ul class="navBar" >
+      <li><a href="index.php">Home</a></li>
+      <li><a href="actualite.php">Actualités</a></li>
+      <li class="menu"><a>Sélections</a>
+         <ul class="sous">
+            <li><a href="#">Paysage</a></li>
+            <li><a href="#">Portrait</a></li>
+            <li><a href="#">Monochrome</a></li>
+         </ul>
+      </li>
+      <li><a href="presentation.php">Présentation</a></li>
+      <li class="menu"><a href="compte.php">Compte</a>
+         <ul class="sous">
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="connexion.php">Ajouter</a></li>
+         </ul>
+      </li>
+   </ul>
 </aside>

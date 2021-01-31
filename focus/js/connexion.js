91c0877b92a1641a2f6eaa9ab800ed132ee4e268
Noé <noephilippe29@gmail.com>
2021-01-31 17:48:39 +0100

    Maj générale

--- focus/js/connexion.js
+++
@@ -1,9 +0,0 @@
-let buttonCreateCompte = document.querySelector('.buttonCreateCompte');
-let connexion = document.querySelector('.connexion');
-let createCompte = document.querySelector('.createCompte');
-
-
-buttonCreateCompte.onclick = function(){
-   createCompte.classList.add('open');
-   connexion.classList.remove('open');
-}
